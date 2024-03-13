19adad93e3f7632273e5ea61f9c528ffb18fb749
LEE SEONGHOON <lar3242@soongsil.ac.kr>
2024-03-13 16:04:04 +0900

    SQL문 + 프론트

---
+++ gifitti_tbl/기프티콘 db.sql
@@ -0,0 +1,113 @@
+
+-- 패스트 푸드
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('맥스파이시 상하이버거 세트', 6900, '맥도날드', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('디지털 상품권 2만원', 20000, '맥도날드', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('불고기버거 세트', 4900, '맥도날드', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('통 모짜와퍼 + 콜라R', 6900, '맥도날드', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('Double X2 버거', 6800, '롯데리아', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('Double X2 세트', 8700, '롯데리아', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('선데아이스크림 스트로베리', 1900, '롯데리아', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('T-REX 세트', 6900, '롯데리아', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('통 모짜와퍼 + 콜라R', 10900, '버거킹', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('치즈와퍼 주니어 + 콜라R', 7000, '버거킹', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('더블비프불고기 버거 + 치즈버거 + 콜라R', 12500, '버거킹', '패스트푸드');
+insert into gifticon_tbl(item_name, price, brand, category) values ('비브불고기버거 + 치즈스틱 + 콜라R', 8500, '버거킹', '패스트푸드');
+
+--  카페
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('시그니처 생딸기 우유생크림케이크' , 33000, '파리바게뜨', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('실키롤케익' , 13000, '파리바게뜨', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('호두 파운드 케익' , 15000, '파리바게뜨', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('가나슈 초콜릿 케이크' , 5200, '파리바게뜨', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('부드러운 정통우유 식빵' , 3400, '파리바게뜨', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('딸기와 우유 생크림 2호' , 32000, '뚜레쥬르', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('올스타 케이크' , 32000, '뚜레쥬르', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('산딸기 라떼 롤케이크' , 15000, '뚜레쥬르', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('클래식 롤케이크' , 14000, '뚜레쥬르', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('까망쿠키 속 리얼말차 브라우니' , 3200, '뚜레쥬르', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('미니글레이즈드 2개 + 미니초코링 2개 + 미니스트로베리필드 2개 ' , 4200, '던킨도너츠', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('먼치킨 10개팩 ' , 5000, '던킨도너츠', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('허니딸기라떼' , 5200, '던킨도너츠', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('베이컨에그 잉글리쉬머핀' , 3500, '던킨도너츠', '베이커리');
+insert into gifticon_tbl(item_name, price, brand, category) values ('약과(45개입)' , 17300, '던킨도너츠', '베이커리');
+
+-- 치킨
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('갈비천왕 +콜라 1.25L', 20500, '굽네치킨', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('고추바사삭+콜라1.25L', 20500, '굽네치킨', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('오리지널+볼케이노+콜라1.25L', 36500, '굽네치킨', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('갈비천왕+시카고피자+콜라1.25L', 38000, '굽네치킨', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('고추바사삭+갈비천왕치밥+콜라1.25L', 26800, '굽네치킨', '치킨/피자');
+
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('(오리지널/순살)후라이드치킨+콜라1.25L', 20000, '노랑통닭', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('(레귤러/순살)칼칼한청양치킨+콜라1.25L', 21000, '노랑통닭', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('(오리지널/순살)노랑3종+떡볶이+오리지널치즈볼+콜라1.25L', 34000, '노랑통닭', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('(오리지널/순살)노랑3종+노랑칩스+콜라1.25L', 26000, '노랑통닭', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('(레귤러/순살)후라이드치킨+깐풍똥집+3종치즈볼+노랑칩스+콜라1.25L', 40000, '노랑통닭', '치킨/피자');
+
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('블랙타이거슈림프(오리지널)L+콜라1.25L', 39200, '도미노피자', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('베스트콰트로피자(오리지널)L+NEW치볼+콜라1.25L', 49000, '도미노피자', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('와일드와일드웨스트 스테이크 오리지날(L)+뉴치볼+콜라1.25L', 46000, '도미노피자', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('블록버스터4(오리지널)L+뉴치볼+콜라1.25L', 49000, '도미노피자', '치킨/피자');
+insert into gifticon_tbl(item_name, price, brand, category) values ('치즈 크레이프 샌드(더블샌드)L+콜라1.25L', 37200, '도미노피자', '치킨/피자');
+
+
+-- 카페a
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('쿨 라임 피지오 Tall', 5900, '스타벅스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('카페 아메리카노 Tall', 4100, '스타벅스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('자바 칩 프라푸치노 Tall', 6300, '스타벅스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('딸기 아사이 레모네이드 스타벅스 리프레셔 T', 5900, '스타벅스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('달콤한 디저트 세트 [카페 아메리카노 T + 7 레이어 가나슈 케이크]', 10200, '스타벅스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('떠먹는 티라미수', 6500, '투썸플레이스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('아메리카노 (R)', 4500, '투썸플레이스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('떠먹는 아이스박스', 6500, '투썸플레이스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('떠먹는 스트로베리 초콜릿 생크림 + 아메리카노 (R)', 11200, '투썸플레이스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('딸기 퐁당 라떼 (R)', 6500, '투썸플레이스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('크랜베리 치킨 샌드위치', 6000, '할리스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('베리베리 딸기 요거트', 6300, '할리스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('민트 초코칩 할리치노 R', 6300, '할리스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('아메리카노R + 바닐라딜라이트R + 트리플초코케이크', 16500, '할리스', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('흑임자 팥붕어빵', 2300, '이디야', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('생과일 수박 리프레싱 모히토 (R)', 4900, '이디야', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('소금빵', 3000, '이디야', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('아메리카노(ICED)', 1800, '빽다방', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('달콤아이스티', 2500, '빽다방', '커피/음료');
+insert into gifticon_tbl(item_name, price, brand, category) values ('초코라떼(ICED)', 3500, '빽다방', '커피/음료');
+
+-- 베라
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('1인 그릭요거트+벌집꿀+그래놀라(배달가능)', 8000, '요거트아이스크림의정석', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('바나나+초코소스+벌집꿀+콘푸라이트+그래놀라+후루트링+1인 요거트아이스크림', 11000, '요거트아이스크림의정석', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('NEW 싱글퀸컵', 4500, '나뚜루', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('뉴 멀티플컵', 13000, '나뚜루', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('미니 스윗 망고', 19800, '나뚜루', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('파인트 아이스크림', 9800, '배스킨라빈스', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('수줍은 웰시코기의 숨바꼭질', 26000, '배스킨라빈스', '아이스크림');
+insert into gifticon_tbl(item_name, price, brand, category) values ('버라이어티팩', 23400, '배스킨라빈스', '아이스크림');
+
+-- 편의점
+
+insert into gifticon_tbl(item_name, price, brand, category) values ('매일)페레로로쉐3구', 2700, 'CU', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('농심)츄파춥스', 300, 'CU', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('하리보골드바렌', 2000, 'CU', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('롯데)ABC초코쿠키50g', 1200, '이마트24', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('육개장사발면', 1000, '이마트24', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('매일)페레로로쉐3구', 2700, '이마트24', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('매일)페레로로쉐3구', 2700, 'GS25', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('서울)비요뜨(초코링)', 2000, 'GS25', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('삼양)불닭볶음면(용기)', 1800, 'GS25', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('PB) 요구르트맛젤리', 1300, '세븐일레븐', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('오뚜기)컵밥쇠고기미역국밥', 4800, '세븐일레븐', '마트/편의점');
+insert into gifticon_tbl(item_name, price, brand, category) values ('롯데)가나밀크2400', 2400, '세븐일레븐', '마트/편의점');
+
+
+
+
+
+
+
+
