d792aec7c396d8b63cfbb0a24e9a81f96af48e70
conchohi <lar3242@gmail.com>
2024-03-25 11:15:08 +0900

    Product, TransController 리팩토링

--- giftiApp/src/main/java/controller/TransController.java
+++ giftiApp/src/main/java/controller/TransController.java
@@ -33,58 +33,90 @@ public class TransController extends HttpServlet {
 		String view = "";
 		
 		if(PATH.equals("/allList.trans")) {
-	         System.out.println("거래 리스트");
-	      
-	         request.setAttribute("transactions", dao.findTransFive());
-	         request.setAttribute("saleList", dao.findSaleAvailFIVE());
-	         view = "/trans/transSaleList.jsp";
+			view = allList(request, response);
 	            
 	      } else if(PATH.equals("/sellInfo.trans")) {
-			
-			int registerId = Integer.parseInt(request.getParameter("registerId"));
-			request.setAttribute("saleItem", dao.findSaleInfo(registerId));
-			view = "/product/saleProductInfo.jsp";
+			view = info(request, response);
 			
 		} else if(PATH.equals("/productSaleList.trans")) {
-			
-			int itemId = Integer.parseInt(request.getParameter("itemId"));
-			request.setAttribute("saleItems", dao.findSaleItem(itemId));
-			view = "/product/productSaleList.jsp";
+			view = productSaleList(request, response);
 			
 		} else if(PATH.equals("/saleDelete.trans")){
-			
-			int registerId = Integer.parseInt(request.getParameter("registerId"));
-			int itemId = dao.getItemId(registerId);
-			dao.deleteSale(registerId);
-			view = "productSaleList.trans?itemId=" + itemId;
+			view = productSaleDelete(request, response);
 			
 		}else if(PATH.equals("/saleDeleteList.trans")){
-			
-			int registerId = Integer.parseInt(request.getParameter("registerId"));
-			dao.deleteSale(registerId);
-			view = "saleList.trans";
+			view = delete(request, response);
 			
 		} else if(PATH.equals("/transList.trans")) {
 			//거래 완료내역
-	         request.setAttribute("transactions", dao.findTrans());
-	         view = "/trans/transList.jsp";
+	        view = transList(request, response);
 		}else if(PATH.equals("/saleList.trans")) {
 			//판매 전체 내역
-	         request.setAttribute("saleList", dao.findSale());
-	         view = "/trans/saleList.jsp";
-	      }else if(PATH.equals("/saleAvailList.trans")) {
-	    	  //판매중
-	         request.setAttribute("saleList", dao.findSaleAvail());
-	         view = "/trans/saleList.jsp";
-	      }else if(PATH.equals("/saleSoldList.trans")) {
+			view = saleList(request, response);
+	         
+	    }else if(PATH.equals("/saleAvailList.trans")) {
+	    	 //판매중
+	    	view = saleAvailList(request, response);
+	         
+	    }else if(PATH.equals("/saleSoldList.trans")) {
 	    	  //판매완료
-		         request.setAttribute("saleList", dao.findSaleSold());
-		         view = "/trans/saleList.jsp";
-		   }
+	    	view = saleSoldList(request, response);
+		         
+		 }
 
-		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
 		dispatcher.forward(request, response);
 	}
-
+	
+	private String allList(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("transactions", dao.findTransFive());
+        request.setAttribute("saleList", dao.findSaleAvailFive());
+        return "/trans/transSaleList.jsp";
+	}
+	
+	private String transList(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("transactions", dao.findTrans());
+        return "/trans/transList.jsp";
+	}
+	
+	private String saleList(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("saleList", dao.findSale());
+        return "/trans/saleList.jsp";
+	}
+	
+	private String saleAvailList(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("saleList", dao.findSaleAvail());
+        return "/trans/saleList.jsp";
+	}
+	
+	private String saleSoldList(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("saleList", dao.findSaleSold());
+	    return "/trans/saleList.jsp";
+	}
+	
+	
+	private String info(HttpServletRequest request, HttpServletResponse response) {
+		int registerId = Integer.parseInt(request.getParameter("registerId"));
+		request.setAttribute("saleItem", dao.findSaleInfo(registerId));
+		return "/product/saleProductInfo.jsp";
+	}
+	
+	private String productSaleList(HttpServletRequest request, HttpServletResponse response) {
+		int itemId = Integer.parseInt(request.getParameter("itemId"));
+		request.setAttribute("saleItems", dao.findSaleItem(itemId));
+		return "/product/productSaleList.jsp";
+	}
+	
+	private String productSaleDelete(HttpServletRequest request, HttpServletResponse response) {
+		int registerId = Integer.parseInt(request.getParameter("registerId"));
+		int itemId = dao.getItemId(registerId);
+		dao.deleteSale(registerId);
+		return "productSaleList.trans?itemId=" + itemId;
+	}
+	
+	private String delete(HttpServletRequest request, HttpServletResponse response) {
+		int registerId = Integer.parseInt(request.getParameter("registerId"));
+		dao.deleteSale(registerId);
+		return "saleList.trans";
+	}
 }
